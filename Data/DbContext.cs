add242f244380f7d0a2d19a1c88f4bbfcf22f6ce
Caio Lara <caio.lara@dtidigital.com.br>
2024-03-11 10:02:22 -0300

    first commit

---
+++ Data/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Produto.Model;
+
+
+namespace Produto.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
